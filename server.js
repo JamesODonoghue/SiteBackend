0e5632baa918cea102d74d75455794e1e4c3676e
James O'Donoghue <james.odonoghue@advent.com>
2018-04-13 12:38:12 -0700

    added my resume to the static files

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ let querystring = require('querystring')
 
 let app = express()
 
+var client_id = 'ceb9be711d7d46d8bdec35c613d38016'; // Your client id
+var client_secret = '655b744453bb4c05867d29aba824a9f5'; // Your secret
+
 let redirect_uri = 
   process.env.REDIRECT_URI || 
   'http://localhost:8080/callback'
@@ -12,8 +15,7 @@ app.get('/login', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
-      client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      client_id: process.env.SPOTIFY_CLIENT_ID || client_id,
       scope: 'user-read-private user-read-email user-top-read',
       redirect_uri
     }))
@@ -34,7 +36,7 @@ app.get('/callback', function(req, res) {
     },
     headers: {
       'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+        (process.env.SPOTIFY_CLIENT_ID || client_id) + ':' + (process.env.SPOTIFY_CLIENT_SECRET || client_secret)
       ).toString('base64'))
     },
     json: true
@@ -45,6 +47,11 @@ app.get('/callback', function(req, res) {
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
+
+// app.use(express.static('assets'));
+
+app.use('/assets',express.static(__dirname + '/assets'));
+
 
 let port = process.env.PORT || 8080
 console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
